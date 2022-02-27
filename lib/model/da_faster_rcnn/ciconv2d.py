2c4a8b7416cadbb8bde9794b70544a8b06c5b0d7
Yanfeng <60517710+teachyourselfcoding@users.noreply.github.com>
2022-02-27 22:43:34 +0800

    Update ciconv2d.py

--- lib/model/da_faster_rcnn/ciconv2d.py
+++ lib/model/da_faster_rcnn/ciconv2d.py
@@ -34,7 +34,7 @@ def gaussian_basis_filters(scale, gpu, k=3):
     x_numpy = x.cpu().numpy()
     x, x_ = np.meshgrid(x_numpy,x_numpy)
     x = torch.from_numpy(x_), torch.from_numpy(x)
-    x.cuda()
+    x.to("cuda:0")
     print("printing x after mesh")
     print(x)
 
