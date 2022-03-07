9ffcc80f8207535e7871ff570dbc30339059e37c
Trajic <e0310446@u.nus.edu>
2022-03-07 16:19:36 +0800

    Update test.py
    vgg16 pretrained=true

--- test.py
+++ test.py
@@ -176,7 +176,7 @@ if __name__ == '__main__':
 
   # initilize the network here.
   if args.net == 'vgg16':
-    fasterRCNN = vgg16(imdb.classes, pretrained=False, class_agnostic=args.class_agnostic)
+    fasterRCNN = vgg16(imdb.classes, pretrained=True, class_agnostic=args.class_agnostic)
   elif args.net == 'res101':
     fasterRCNN = resnet(imdb.classes, 101, pretrained=False, class_agnostic=args.class_agnostic)
   elif args.net == 'res50':
