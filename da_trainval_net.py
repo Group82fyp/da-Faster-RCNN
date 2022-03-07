4afe45d1d024df82fa0aaa9bc8e5b1b53b3c0a31
Trajic <e0310446@u.nus.edu>
2022-03-07 17:16:32 +0800

    Update da_trainval_net.py
    changed location of fasterrcnn.cuda

--- da_trainval_net.py
+++ da_trainval_net.py
@@ -331,7 +331,12 @@ if __name__ == '__main__':
 
   elif args.optimizer == "sgd":
     optimizer = torch.optim.SGD(params, momentum=cfg.TRAIN.MOMENTUM)
+    print('optimizer')
+    print(optimizer)
 
+  if args.cuda:
+    fasterRCNN.cuda()
+      
   if args.resume:
     # load_name = os.path.join(output_dir,
     #   'faster_rcnn_{}_{}_{}.pth'.format(args.checksession, args.checkepoch, args.checkpoint))
@@ -356,8 +361,7 @@ if __name__ == '__main__':
   if args.mGPUs:
     fasterRCNN = nn.DataParallel(fasterRCNN)
 
-  if args.cuda:
-    fasterRCNN.cuda()
+
 
   iters_per_epoch = int(s_train_size / args.batch_size)
 
