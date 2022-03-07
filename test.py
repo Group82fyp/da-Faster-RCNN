cc327f238842226e959e2cf0abab7ff1075f73fb
Trajic <e0310446@u.nus.edu>
2022-03-07 16:28:44 +0800

    pretrained=False
    pretrained=False, test.py

--- test.py
+++ test.py
@@ -176,7 +176,7 @@ if __name__ == '__main__':
 
   # initilize the network here.
   if args.net == 'vgg16':
-    fasterRCNN = vgg16(imdb.classes, pretrained=True, class_agnostic=args.class_agnostic)
+    fasterRCNN = vgg16(imdb.classes, pretrained=False, class_agnostic=args.class_agnostic)
   elif args.net == 'res101':
     fasterRCNN = resnet(imdb.classes, 101, pretrained=False, class_agnostic=args.class_agnostic)
   elif args.net == 'res50':
@@ -197,8 +197,8 @@ if __name__ == '__main__':
   print("fasterRCNN.state_dict()")
   print(fasterRCNN.state_dict())
 
-  fasterRCNN.load_state_dict({k: v for k, v in checkpoint['model'].items() if k in fasterRCNN.state_dict()})
-  #fasterRCNN.load_state_dict(checkpoint['model'])
+  # fasterRCNN.load_state_dict({k: v for k, v in checkpoint['model'].items() if k in fasterRCNN.state_dict()})
+  fasterRCNN.load_state_dict(checkpoint['model'])
   if 'pooling_mode' in checkpoint.keys():
     cfg.POOLING_MODE = checkpoint['pooling_mode']
 
