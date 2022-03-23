03463b31ecdb11dff25a776efa03ff726b97be62
Trajic <e0310446@u.nus.edu>
2022-03-23 23:45:53 +0800

    Update vgg16.py

--- lib/model/da_faster_rcnn/vgg16.py
+++ lib/model/da_faster_rcnn/vgg16.py
@@ -41,7 +41,7 @@ class vgg16(_fasterRCNN):
     # Fix the layers before conv3:
     # for layer in range(10):
     #   print("printing layer: ", layer, ", printing name: ", self.RCNN_base[layer])
-    for layer in range(1,10):
+    for layer in range(10):
       print("printing layer: ", layer, ", printing name: ", self.RCNN_base[layer])
       for p in self.RCNN_base[layer].parameters(): p.requires_grad = True
 
