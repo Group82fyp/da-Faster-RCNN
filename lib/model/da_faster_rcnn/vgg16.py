8cd4a0e9ed76969a51fc61b3688a085815c0c65a
Trajic <e0310446@u.nus.edu>
2022-03-23 23:37:20 +0800

    Update vgg16.py

--- lib/model/da_faster_rcnn/vgg16.py
+++ lib/model/da_faster_rcnn/vgg16.py
@@ -42,7 +42,7 @@ class vgg16(_fasterRCNN):
     # for layer in range(10):
     #   print("printing layer: ", layer, ", printing name: ", self.RCNN_base[layer])
     for layer in range(1,10):
-      # print("printing layer: ", layer, ", printing name: ", self.RCNN_base[layer])
+      print("printing layer: ", layer, ", printing name: ", self.RCNN_base[layer])
       for p in self.RCNN_base[layer].parameters(): p.requires_grad = True
 
     # self.RCNN_base = _RCNN_base(vgg.features, self.classes, self.dout_base_model)
