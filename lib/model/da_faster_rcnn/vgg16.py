4afe5e3e39b9d7484d899936968f9c2131fd0fbf
Trajic <e0310446@u.nus.edu>
2022-03-27 19:11:26 +0800

    updated save-weights name

--- lib/model/da_faster_rcnn/vgg16.py
+++ lib/model/da_faster_rcnn/vgg16.py
@@ -20,7 +20,7 @@ from model.da_faster_rcnn.ciconv2d import CIConv2d
 class vgg16(_fasterRCNN):
   def __init__(self, classes, pretrained=False, class_agnostic=False):
     self.model_path = 'data/vgg16_caffe.pth'
-    self.base_model_path = './trained_model/vgg16/cityscape/bdd100k_ciconv.pth'
+    self.new_model_path = './trained_model/vgg16/cityscape/24_4_240.pth'
     self.dout_base_model = 512
     self.pretrained = pretrained
     self.class_agnostic = class_agnostic
@@ -39,16 +39,14 @@ class vgg16(_fasterRCNN):
     # self.RCNN_base = nn.Sequential(*list(vgg.features._modules.values())[:-1])
     preprocessing = nn.Sequential(CIConv2d('W'), nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False))
     vgg.preprocessing[0] = preprocessing
-    self.RCNN_base = vgg.preprocessing
+
 
     # self.RCNN_base[0] = preprocessing
     # Fix the layers before conv3:
     # for layer in range(10):
     #   print("printing layer: ", layer, ", printing name: ", self.RCNN_base[layer])
 
-    for layer in range(10):
-      print("printing layer: ", layer, ", printing name: ", vgg.preprocessing[layer])
-      for p in self.RCNN_base[layer].parameters(): p.requires_grad = True
+
 
     # self.RCNN_base = _RCNN_base(vgg.features, self.classes, self.dout_base_model)
     # print("printing entire rcnnbase")
@@ -63,12 +61,16 @@ class vgg16(_fasterRCNN):
     #     self.RCNN_base.load_state_dict(base_state_dict, strict=False)
 
     if self.pretrained:
-        print("Loading pretrained weights from %s" %(self.model_path))
-        state_dict = torch.load(self.model_path)
+        print("Loading pretrained weights from %s" %(self.new_model_path))
+        state_dict = torch.load(self.new_model_path)
         vgg.load_state_dict({k:v for k,v in state_dict.items() if k in vgg.state_dict()}, strict=False)
 
-
+    self.RCNN_base = vgg.preprocessing
     self.RCNN_top = vgg.classifier
+
+    for layer in range(10):
+      print("printing layer: ", layer, ", printing name: ", vgg.preprocessing[layer])
+      for p in self.RCNN_base[layer].parameters(): p.requires_grad = True
 
     # not using the last maxpool layer
     self.RCNN_cls_score = nn.Linear(4096, self.n_classes)
