63a48c92d9fad0911a73fd1e55d25d9856f71666
Trajic <e0310446@u.nus.edu>
2022-03-30 10:51:37 +0800

    ..

--- lib/model/faster_rcnn/vgg16.py
+++ lib/model/faster_rcnn/vgg16.py
@@ -49,7 +49,12 @@ class vgg16(_fasterRCNN):
     self.RCNN_base = nn.Sequential(*list(vgg.features._modules.values())[:-1])
 
     # Fix the layers before conv3:
+    for layer in range(1,10):
+      # print("printing layer: ", layer, ", printing name: ", self.RCNN_base[layer])
+      for p in self.ciconv_RCNN_base[layer].parameters(): p.requires_grad = False
+
     for layer in range(10):
+      # print("printing layer: ", layer, ", printing name: ", self.RCNN_base[layer])
       for p in self.RCNN_base[layer].parameters(): p.requires_grad = False
 
     # self.RCNN_base = _RCNN_base(vgg.features, self.classes, self.dout_base_model)
