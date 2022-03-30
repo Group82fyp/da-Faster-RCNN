e6e88a5ee1123e7e4d73c275370f3db89790a58e
Trajic <e0310446@u.nus.edu>
2022-03-30 15:23:48 +0800

    Update vgg16.py

--- lib/model/faster_rcnn/vgg16.py
+++ lib/model/faster_rcnn/vgg16.py
@@ -43,11 +43,8 @@ class vgg16(_fasterRCNN):
                                   nn.Conv2d(4, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False))
     vgg.preprocessing[0] = preprocessing
 
-
-
-
-    # not using the last maxpool layer
-    self.RCNN_base = vgg.preprocessing
+    self.ciconv_RCNN_base = vgg.preprocessing
+    self.RCNN_base = nn.Sequential(*list(vgg.features._modules.values())[:-1])
 
     # Fix the layers before conv3:
     for layer in range(1,10):
